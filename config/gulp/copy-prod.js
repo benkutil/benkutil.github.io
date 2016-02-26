a3cdde5b3fa76859e2cd7b3a0a038f856e39d50a
Ben Kutil <ben@make-things.com>
2016-02-25 23:36:53 -0500

    :runner: fix build process

--- config/gulp/copy-prod.js
+++ config/gulp/copy-prod.js
@@ -2,9 +2,8 @@ module.exports = ( gulp, $, config ) => {
   gulp.task( 'copy-prod', () => {
 
     return gulp.src([
-      'tmp/*.*',
+      'tmp/**/*.*',
       '!tmp/**/*.html',
-      'shared/**'
     ], {
       dot: true
     }).pipe(gulp.dest('dist'));
