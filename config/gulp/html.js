e771451e415ad4e3f19d288441d27fd7ccfdea38
Ben Kutil <ben@make-things.com>
2016-03-14 23:23:00 -0400

    :zap: add uncss

--- config/gulp/html.js
+++ config/gulp/html.js
@@ -1,14 +1,19 @@
 module.exports = (gulp, $, config) => {
     gulp.task('html', () => {
+        const uncssConfig = {
+            html: [ 'tmp/**/*.html' ]
+        }
+        const htmlminConfig = {
+            collapseWhitespace: true
+        };
+
         return gulp.src('tmp/**/*.html')
             .pipe($.useref({
                 searchPath: ['tmp', 'source', './']
             }))
             .pipe($.if('*.js', $.uglify()))
-            .pipe($.if('*.css', $.cssnano()))
-            .pipe($.if('*.html', $.htmlmin({
-                collapseWhitespace: true
-            })))
+            .pipe( $.if( '*.css', $.uncss( uncssConfig ), $.cssnano() ) )
+            .pipe( $.if( '*.html', $.htmlmin( htmlminConfig ) ) )
             .pipe(gulp.dest('dist'));
     });
 }
