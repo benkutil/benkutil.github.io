805c416d966aa05f8043c0e76df2b2f1ef65c18f
Ben Kutil <thisis@benkutil.com>
2022-12-19 00:27:30 +0000

    update 11ty to use njk rss plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
-const xmlFiltersPlugin = require('eleventy-xml-plugin')
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPlugin(xmlFiltersPlugin);
+    eleventyConfig.addPlugin(pluginRss);
+    
     // Directory changes
     return {
         dir: {
