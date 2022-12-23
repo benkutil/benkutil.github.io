7d955d24ae4070aa8efbeafe93be5c3984439e2b
Ben Kutil <thisis@benkutil.com>
2022-12-23 01:25:11 +0000

    add conditional build statement

--- .eleventy.js
+++ .eleventy.js
@@ -4,21 +4,25 @@ const htmlmin = require("html-minifier");
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(pluginRss);
     
-    eleventyConfig.addTransform("htmlmin",  function(content,  outputPath) {
-        // find html files
-        if(outputPath && outputPath.endsWith(".html")) {
-            // configure html-minify
-            let minified = htmlmin.minify(content, {
-                useShortDoctype: true,
-                removeComments: true,
-                minifyCSS: true,
-                collapseWhitespace: true
-            });
-            return minified;
-        }
-        
-        return content;
-    });
+    // run these configs in production only
+    if (process.env.ELEVENTY_ENV === 'production' ) {
+        eleventyConfig.addTransform("htmlmin",  function(content,  outputPath) {
+            // find html files
+            if(outputPath && outputPath.endsWith(".html")) {
+                // configure html-minify
+                let minified = htmlmin.minify(content, {
+                    useShortDoctype: true,
+                    removeComments: true,
+                    minifyCSS: true,
+                    collapseWhitespace: true
+                });
+                return minified;
+            }
+            
+            return content;
+        });
+    
+    }
 
     // Directory changes
     return {
