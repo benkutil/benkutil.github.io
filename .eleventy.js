81e93678d37305e2b58a0047443afdcbaa3e3176
Ben Kutil <thisis@benkutil.com>
2022-12-23 01:05:41 +0000

    add eleventy transform for minifying output

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,21 @@ const htmlmin = require("html-minifier");
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(pluginRss);
     
+    eleventyConfig.addTransform("htmlmin",  function(content,  outputPath) {
+        // find html files
+        if(outputPath && outputPath.endsWith(".html")) {
+            // configure html-minify
+            let minified = htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true
+            });
+            return minified;
+        }
+        
+        return content;
+    });
+
     // Directory changes
     return {
         dir: {
