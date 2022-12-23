81566bfbaaa015bc867a03b69296cb9f8a207fc9
Ben Kutil <thisis@benkutil.com>
2022-12-22 20:38:56 -0500

    :hammer: minify production output (#81)
    Closes #70 
    
    - ➕ add `html-minifier` package
    - 🔧 reference `html-minifer` package
    - 🔧 add eleventy transform for minifying output
    - 🔧 update html-minifer config to minify - minify css in style attributes
    - 🔧  add conditional build statement
    - 🔨 add production build script

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,29 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const htmlmin = require("html-minifier");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(pluginRss);
     
+    // run these configs in production only
+    if (process.env.ELEVENTY_ENV === 'production' ) {
+        eleventyConfig.addTransform("htmlmin",  function(content,  outputPath) {
+            // find html files
+            if(outputPath && outputPath.endsWith(".html")) {
+                // configure html-minify
+                let minified = htmlmin.minify(content, {
+                    useShortDoctype: true,
+                    removeComments: true,
+                    minifyCSS: true,
+                    collapseWhitespace: true
+                });
+                return minified;
+            }
+            
+            return content;
+        });
+    
+    }
+
     // Directory changes
     return {
         dir: {
